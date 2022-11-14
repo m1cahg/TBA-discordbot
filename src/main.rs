afa823eabfe5a10173f63ca4c8e4952fecec92ab
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-11-13 23:04:34 -0500

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
 use std::env;
 
 use serenity::async_trait;
-use serenity::prelude::*;
-use serenity::model::channel::Message;
 use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{StandardFramework, CommandResult};
-use tracing::{error, info};
+use serenity::framework::standard::{CommandResult, StandardFramework};
+use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
-
+use serenity::prelude::*;
+use tracing::{error, info};
 
 #[group]
 #[commands(ping)]
@@ -19,7 +21,17 @@ struct Handler;
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content.chars().nth(0).unwrap() == '#' {
-            if let Err(e) = msg.channel_id.say(&ctx.http, format!("https://www.thebluealliance.com/team/{0}",&msg.content[1..])).await {
+            if let Err(e) = msg
+                .channel_id
+                .say(
+                    &ctx.http,
+                    format!(
+                        "https://www.thebluealliance.com/team/{0}",
+                        &msg.content[1..]
+                    ),
+                )
+                .await
+            {
                 error!("Error sending message: {:?}", e);
             }
         }
@@ -30,16 +42,16 @@ impl EventHandler for Handler {
     }
 }
 
-
-
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
 
     // Login with a bot token from the environment
-    let token = "MTAzNTE3MDMyOTY0OTIzMzk1MA.GpdhLj.vV7oEgF7p7qmGBTp1xqWp-3UebWEa5pfamVivw";
+    let token = env::var("DISCORD_API_TOKEN").expect("$DISCORD_API_TOKEN is not set");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
