490262cf6cb951191c718d5de520ce30c8655d1e
m1cahg <goodmanmicah1@gmail.com>
2022-11-13 23:07:01 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use std::env;
+
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, CommandResult};
+use tracing::{error, info};
+use serenity::model::gateway::Ready;
+
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content.chars().nth(0).unwrap() == '#' {
+            if let Err(e) = msg.channel_id.say(&ctx.http, format!("https://www.thebluealliance.com/team/{0}",&msg.content[1..])).await {
+                error!("Error sending message: {:?}", e);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+    }
+}
+
+
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = "MTAzNTE3MDMyOTY0OTIzMzk1MA.GpdhLj.vV7oEgF7p7qmGBTp1xqWp-3UebWEa5pfamVivw";
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "test").await?;
+
+    Ok(())
+}
